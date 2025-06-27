d5e55ad6b552fee39ef02ecc53a920da6e28e01c
expresstaxisept <expresstaxisept@gmail.com>
2025-06-27 11:57:47 +0100

    updates

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $value = htmlspecialchars($_POST['test']);
-  echo "✅ POST reçu avec succès !<br>Valeur reçue : " . $value;
-} else {
-  echo "❌ Cette page accepte uniquement les requêtes POST.";
-}
-?>
